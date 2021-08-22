f9a6563686cd6980109c6eb38b967c0a1fbd1fd3
Leon Si <leonzalion@gmail.com>
2021-08-22 17:27:21 -0400

    add frontend code

--- frontend/src/pages/SubmitSolution.vue
+++ frontend/src/pages/SubmitSolution.vue
@@ -1,7 +1,22 @@
 <template>
-	<form action="/run" method="post" enctype="multipart/form-data">
-		<label for="file">Upload and test an HTML file</label><br />
-		<input type="file" id="file" name="file" /><br />
-		<input type="submit" value="Submit" /><br />
-	</form>
+	<label for="file">Upload and test an HTML file</label><br />
+	<input type="file" id="file" name="file" /><br />
+	<button @click="submitCode">Submit</button>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { serverKy } from '~/utils/ky';
+
+export default defineComponent({
+	setup() {
+		async function submitCode() {
+			await serverKy.post('/run');
+		}
+
+		return {
+			submitCode,
+		};
+	},
+});
+</script>
