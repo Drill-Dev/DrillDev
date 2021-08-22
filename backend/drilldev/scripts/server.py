136152d21b57f0ff8206a363a35bad3f25571c89
Leon Si <leonzalion@gmail.com>
2021-08-22 16:50:13 -0400

    fixed python issues

--- backend/drilldev/scripts/server.py
+++ backend/drilldev/scripts/server.py
@@ -2,6 +2,6 @@ import os
 
 def main():
 	os.system("""
-		FLASK_APP=src/app.py FLASK_DEBUG=1 \
+		FLASK_APP=drilldev/app.py FLASK_DEBUG=1 \
 			poetry run flask run --host=0.0.0.0 --port=5000
 	""")
