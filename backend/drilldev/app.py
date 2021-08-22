47a7b10d939e74a4aa5f2fd2672fdb76fb7ffc12
Leon Si <leonzalion@gmail.com>
2021-08-22 16:59:35 -0400

    use tabs for python

--- backend/drilldev/app.py
+++ backend/drilldev/app.py
@@ -5,37 +5,40 @@ import playwright.sync_api
 
 app = flask.Flask(__name__)
 
+
 @app.route("/")
 def root():
-    return '''
-        <form action="/run" method="post" enctype="multipart/form-data">
-            <label for="file">Upload and test an HTML file</label><br />
-            <input type="file" id="file" name="file" /><br />
-            <input type="submit" value="Submit" /><br />
-        </form>
-    '''
+	return '''
+		<form action="/run" method="post" enctype="multipart/form-data">
+			<label for="file">Upload and test an HTML file</label><br />
+			<input type="file" id="file" name="file" /><br />
+			<input type="submit" value="Submit" /><br />
+		</form>
+	'''
+
 
 @app.route("/run", methods=["POST"])
 def run():
-    flask.request.files["file"].save("file.html")
-    with playwright.sync_api.sync_playwright() as p:
-        browser = p.firefox.launch()
-        page = browser.new_page()
-        page.goto(f"file:///{os.getcwd()}/file.html")
-        page.set_default_timeout(3000)  # 3 seconds
-        try:
-            page.click("text='Login'")
-        except BaseException as e:  # TODO: Replace with TimeoutError later
-            print(repr(e))
-            return f'''
-                <p>Failed</p>
-                <a href="{flask.url_for("root")}">Home</a>
-            '''
-        browser.close()
-    return f'''
-        <p>Passed</p>
-        <a href="{flask.url_for("root")}">Home</a>
-    '''
+	flask.request.files["file"].save("file.html")
+	with playwright.sync_api.sync_playwright() as p:
+		browser = p.firefox.launch()
+		page = browser.new_page()
+		page.goto(f"file:///{os.getcwd()}/file.html")
+		page.set_default_timeout(3000)  # 3 seconds
+		try:
+			page.click("text='Login'")
+		except BaseException as e:  # TODO: Replace with TimeoutError later
+			print(repr(e))
+			return f'''
+				<p>Failed</p>
+				<a href="{flask.url_for("root")}">Home</a>
+			'''
+		browser.close()
+	return f'''
+		<p>Passed</p>
+		<a href="{flask.url_for("root")}">Home</a>
+	'''
+
 
 if __name__ == "__main__":
-    app.run()
+	app.run()
