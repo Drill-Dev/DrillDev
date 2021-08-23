1d995efa7cb912d013bf63e564ae2e509eaf4d42
George Zhang <geetransit@gmail.com>
2021-08-22 21:30:21 -0400

    Run the server with FLASK_ENV set to development

--- backend/scripts/server.py
+++ backend/scripts/server.py
@@ -3,6 +3,7 @@ import os
 
 def main():
 	os.environ["FLASK_APP"] = "drilldev/app.py"
+	os.environ["FLASK_ENV"] = "development"
 	os.environ["FLASK_DEBUG"] = "1"
 	if os.path.isfile("/.dockerenv"):
 		os.system("flask run --host=0.0.0.0 --port=5000")
