672c158d5d158521e21b6a3d2f177954fd5e0a88
George Zhang <geetransit@gmail.com>
2021-08-23 12:02:58 -0400

    Make sure the submission container is closed

--- backend/src/routes/drill/submit.ts
+++ backend/src/routes/drill/submit.ts
@@ -50,61 +50,63 @@ export default async function drillSubmitRoute(app: FastifyInstance) {
 
 		await submissionContainer.start();
 
-		// A function for running the Playwright test
-		async function runTest(browser: playwright.Browser) {
-			const page = await browser.newPage();
-
-			await page.goto('http://localhost:8080');
-			await page.setDefaultTimeout(3000);
-			await page.click("text='Login'");
-		}
-
 		try {
-			// Give the page 5 seconds to bind to port 8080
-			await promiseRetry(
-				async (retry) => {
-					const { statusCode } = await got.get('http://localhost:8080');
-					if (!(statusCode >= 200 && statusCode < 400)) {
-						retry(new Error('Could not bind to port.'));
-					}
-				},
-				{ minTimeout: 1000, factor: 1, retries: 5 }
-			);
-		} catch (e) {
-			// Send a TLE if they're taking too long to bind to the port
-			return await reply.send({
-				status: 'PE',
-			});
-		}
+			// A function for running the Playwright test
+			async function runTest(browser: playwright.Browser) {
+				const page = await browser.newPage();
 
-		// Launch a playwright browser
-		const browser = await playwright.chromium.launch();
-		try {
-			// Run the test
-			await runTest(browser);
+				await page.goto('http://localhost:8080');
+				await page.setDefaultTimeout(3000);
+				await page.click("text='Login'");
+			}
 
-			// Send an AC status if the test passes without errors
-			return reply.send({ status: 'AC' });
-		} catch (e) {
-			console.error(e);
-
-			// Send a TLE if the error is a TimeoutError
-			if (e instanceof playwright.errors.TimeoutError) {
-				return reply.send({
-					status: 'TLE',
+			try {
+				// Give the page 5 seconds to bind to port 8080
+				await promiseRetry(
+					async (retry) => {
+						const { statusCode } = await got.get('http://localhost:8080');
+						if (!(statusCode >= 200 && statusCode < 400)) {
+							retry(new Error('Could not bind to port.'));
+						}
+					},
+					{ minTimeout: 1000, factor: 1, retries: 5 }
+				);
+			} catch (e) {
+				// Send a TLE if they're taking too long to bind to the port
+				return await reply.send({
+					status: 'PE',
 				});
 			}
 
-			// Otherwise, send an IE (internal error)
-			else {
-				return reply.send({
-					status: 'IE',
-				});
+			// Launch a playwright browser
+			const browser = await playwright.chromium.launch();
+			try {
+				// Run the test
+				await runTest(browser);
+
+				// Send an AC status if the test passes without errors
+				return reply.send({ status: 'AC' });
+			} catch (e) {
+				console.error(e);
+
+				// Send a TLE if the error is a TimeoutError
+				if (e instanceof playwright.errors.TimeoutError) {
+					return reply.send({
+						status: 'TLE',
+					});
+				}
+
+				// Otherwise, send an IE (internal error)
+				else {
+					return reply.send({
+						status: 'IE',
+					});
+				}
+			} finally {
+				// Close the browser
+				await browser.close();
 			}
 		} finally {
-			// Close the browser
-			await browser.close();
-
 			// Destroy the submission container
 			await submissionContainer.remove({
 				force: true,
