53e7dfb9c5828c8baf835419d056bc1e10acc595
George Zhang <geetransit@gmail.com>
2021-08-23 23:46:07 -0400

    Add /drill/create route

--- backend/src/routes/drill/create.ts
+++ backend/src/routes/drill/create.ts
@@ -1,5 +1,16 @@
 import { FastifyInstance } from 'fastify';
 
-export default async function drillCreateRoute(_app: FastifyInstance) {
-	// TODO
+export default async function drillCreateRoute(app: FastifyInstance) {
+	app.post('/drill/create', async (request, reply) => {
+		const fileData = await request.file();
+		const codeBuffer = await fileData.toBuffer();
+		const code = codeBuffer.toString("utf-8");
+		const { id } = await request.prisma.drill.create({
+			data: {
+				judgingCode: code,
+				judgingOptions: {},
+			},
+		});
+		await reply.send({ id });
+	});
 }
