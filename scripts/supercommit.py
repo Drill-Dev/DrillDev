1d6e7774c997e21eaa2191729fe0950982024c22
George Zhang <geetransit@gmail.com>
2021-08-26 21:29:39 -0400

    fix: pass lists to check_call

--- scripts/supercommit.py
+++ scripts/supercommit.py
@@ -7,8 +7,8 @@ if len(sys.argv) < 2:
     sys.exit("You need to provide a commit message")
 msg = sys.argv[1]
 
-subprocess.check_call("git submodule foreach git add -A .")
-subprocess.check_call(f'git submodule foreach git commit -am "{msg}"')
+subprocess.check_call("git submodule foreach git add -A .".split())
+subprocess.check_call("git submodule foreach git commit -am".split() + [msg])
 
-subprocess.check_call("git add -A .")
-subprocess.check_call(f'git commit -am "{msg}"')
+subprocess.check_call("git add -A .".split())
+subprocess.check_call("git commit -am".split() + [msg])
--- scripts/superpush.py
+++ scripts/superpush.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 
 import subprocess
-subprocess.check_call("git submodule foreach git push")
-subprocess.check_call("git push")
+subprocess.check_call("git submodule foreach git push".split())
+subprocess.check_call("git push".split())
