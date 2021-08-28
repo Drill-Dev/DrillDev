00f49184e1f1c55113921eb111c9410a57eef12c
Leon Si <leonzalion@gmail.com>
2021-08-28 01:31:31 -0400

    fix: superpull script

--- scripts/superpull.py
+++ scripts/superpull.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 
 import subprocess
+subprocess.check_call("git submodule foreach git pull".split())
 subprocess.check_call("git pull".split())
-subprocess.check_call("git submodule update --remote".split())
