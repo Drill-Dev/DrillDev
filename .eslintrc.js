6da694987e1bc240c7ad57be8d73eebef3121535
Leon Si <leonzalion@gmail.com>
2021-08-28 12:59:19 -0400

    chore: added eslint airbnb config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@
 
 module.exports = {
 	extends: [
+		'airbnb-base',
 		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended',
 		'prettier',
@@ -9,33 +10,10 @@ module.exports = {
 	],
 	plugins: ['@typescript-eslint', 'simple-import-sort', 'import', 'unicorn'],
 	rules: {
-		'simple-import-sort/imports': 'error',
-		'simple-import-sort/exports': 'error',
+		// eslint rules
 		'no-unused-vars': 'off',
-		'@typescript-eslint/explicit-module-boundary-types': 'off',
-		'@typescript-eslint/explicit-function-return-type': 'off',
-		'@typescript-eslint/no-unused-vars': 'off',
-		'@typescript-eslint/no-unused-vars-experimental': [
-			'error',
-			{ ignoreArgsIfArgsAfterAreUsed: true },
-		],
 		'no-debugger': 'off',
 		'no-param-reassign': 'off',
-		'import/extensions': [
-			'error',
-			'ignorePackages',
-			{
-				js: 'never',
-				ts: 'never',
-			},
-		],
-		'no-lonely-if': 'off',
-		'no-void': 'off',
-		'no-underscore-dangle': 'off',
-		'no-unused-expressions': 'off',
-		'@typescript-eslint/no-unused-expressions': 'error',
-		'no-shadow': 'off',
-		'@typescript-eslint/no-shadow': 'error',
 		'prefer-destructuring': [
 			'error',
 			{
@@ -44,9 +22,67 @@ module.exports = {
 		],
 		'no-await-in-loop': 'off',
 		'no-else-return': 'off',
+		'no-lonely-if': 'off',
+		'no-void': 'off',
+		'no-underscore-dangle': 'off',
+		'no-unused-expressions': 'off',
+		'no-shadow': 'off',
+
+		// eslint-plugin-import rules
+		'import/no-default-export': 'error',
+		'import/prefer-default-export': 'off',
+		'import/extensions': [
+			'error',
+			'ignorePackages',
+			{
+				js: 'never',
+				ts: 'never',
+			},
+		],
+
+		// eslint-config-airbnb-base style overrides
+		'no-restricted-syntax': [
+			'error',
+			{
+				selector: 'ForInStatement',
+				message:
+					'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+			},
+			{
+				selector: 'ForOfStatement',
+				message:
+					'iterators/generators require regenerator-runtime, which is too heavyweight for this guide to allow them. Separately, loops should be avoided in favor of array iterations.',
+			},
+			{
+				selector: 'LabeledStatement',
+				message:
+					'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+			},
+			{
+				selector: 'WithStatement',
+				message:
+					'`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+			},
+		],
+
+		// eslint-plugin-simple-import-sort rules
+		'simple-import-sort/imports': 'error',
+		'simple-import-sort/exports': 'error',
+
+		// TypeScript ESLint rules
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'@typescript-eslint/no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars-experimental': [
+			'error',
+			{ ignoreArgsIfArgsAfterAreUsed: true },
+		],
+		'@typescript-eslint/no-unused-expressions': 'error',
+		'@typescript-eslint/no-shadow': 'error',
 		'@typescript-eslint/no-explicit-any': 'off',
 		'@typescript-eslint/no-non-null-assertion': 'off',
-		'import/no-default-export': 'error',
+
+		// eslint-plugin-unicorn
 		'unicorn/prefer-module': 'off',
 		'unicorn/prefer-node-protocol': 'off',
 		'unicorn/prefer-ternary': 'off',
