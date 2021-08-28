15e15e0370b8f2b90792d7003e6d16f6c55cece4
Leon Si <leonzalion@gmail.com>
2021-08-28 02:17:17 -0400

    fix: updated scripts

--- scripts/supercommit.py
+++ scripts/supercommit.py
@@ -5,9 +5,13 @@ import subprocess
 
 if len(sys.argv) < 2:
     sys.exit("You need to provide a commit message")
-msg = sys.argv[1]
+args = sys.argv[1:]
 
-subprocess.check_call("git submodule foreach git commit -m".split() + [msg])
+try:
+    subprocess.check_call("git submodule foreach git commit".split() + args)
+except:
+    pass
+
 
 subprocess.check_call("git add backend".split())
-subprocess.check_call("git commit -m".split() + [msg])
+subprocess.check_call("git commit".split() + args)
