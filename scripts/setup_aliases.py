184c9a8628289f57359c1f1f5617ea5fa0a6af27
Leon Si <leonzalion@gmail.com>
2021-08-28 02:55:45 -0400

    feat: added update aliases script

--- scripts/setup_aliases.py
+++ scripts/setup_aliases.py
@@ -3,12 +3,14 @@
 import pathlib
 
 cwd = pathlib.Path.cwd()
-add = '''\
-[include]
-	path = ../.gitalias
-'''
 
 with open(cwd / ".git/config", mode="a") as file:
-	file.write(add)
+	file.write('''\
+		[include]
+			path = ../.gitalias
+	''')
 with open(cwd / ".git/modules/backend/config", mode="a") as file:
-	file.write(add)
+	file.write('''
+		[include]
+			path = ../../../.gitalias
+	''')
---
+++ scripts/update_aliases.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import pathlib
+import re
+
+cwd = pathlib.Path.cwd()
+aliases = open(cwd / '.gitalias', 'r').read()
+
+def update_config(config_path: str, path_prefix: str = None):
+	old_config = open(config_path, "r").read()
+
+	# Clear the file
+	file = open(config_path, 'w')
+
+	# Delete any old aliases from the config
+	remove_alias_re = re.compile(r'\[alias\].*(\[|$)', re.DOTALL)
+	new_config = re.sub(remove_alias_re, '', old_config)
+
+	config_aliases = aliases
+	if path_prefix != None:
+		assign_alias_re = re.compile(r'(=\s*"!\w+\s+)')
+		config_aliases = re.sub(assign_alias_re, r'\1' + f'{path_prefix}/', aliases)
+
+	# Append the most recent aliases (from the .gitalias file)
+	new_config += config_aliases
+
+	# Overwrite the config file with the new config
+	file.write(new_config)
+
+
+update_config(cwd / ".git/config")
+update_config(cwd / ".git/modules/backend/config", path_prefix="..")
