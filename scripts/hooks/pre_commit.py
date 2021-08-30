d7dd269dfb2f6dc685eb2cb10b4ff084d792f1a6
Leon Si <leonzalion@gmail.com>
2021-08-30 16:56:38 -0400

    fix: python script

--- scripts/hooks/pre_commit.py
+++ scripts/hooks/pre_commit.py
@@ -1,10 +1,10 @@
 import shlex
 import subprocess
 
-branch = subprocess.check_output(
-    shlex.split("git rev-parse --symbolic-full-name --abbrev-ref HEAD"))
-
-print(branch)
+branch = str.strip(
+    subprocess.check_output(
+        shlex.split("git rev-parse --symbolic-full-name --abbrev-ref HEAD")).
+    decode('utf-8'))
 
 if branch == "dev":
 	subprocess.check_call(shlex.split("yarn run lint-staged"))
