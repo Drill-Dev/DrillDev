81b39a919ace22d060a9f33eac3e148ff089312a
Dragon <45088170+dragondevelops@users.noreply.github.com>
2021-09-01 13:01:53 -0400

    feat(frontend): login improvements

--- frontend/src/pages/account/login-page.vue
+++ frontend/src/pages/account/login-page.vue
@@ -1,28 +1,43 @@
 <template lang="pug">
 AuthLayout(title="Login to DrillDev")
-	AuthInputBox(title="Email", type="text", v-model="email")
+	AuthInputBox(title="Email", type="text", v-model="usernameOrEmail")
 	AuthInputBox(title="Password", type="password", v-model="password")
 	AuthButton.bg-blue-700.text-white.transition-all.duration-25(
 		title="Login",
-		class="hover:bg-blue-900"
+		class="hover:bg-blue-900",
+		@click="onLogin"
 	)
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import validator from 'email-validator';
+import { computed, defineComponent, ref } from 'vue';
 
-import AuthButton from '~/components/auth-button.vue';
-import AuthInputBox from '~/components/auth-input-box.vue';
-import AuthLayout from '~/components/auth-layout.vue';
+import AuthButton from '~/components/AuthButton.vue';
+import AuthInputBox from '~/components/AuthInputBox.vue';
+import AuthLayout from '~/components/AuthLayout.vue';
+import { serverKy } from '~/utils/ky';
 
 export default defineComponent({
 	name: 'LoginPage',
 	components: { AuthLayout, AuthInputBox, AuthButton },
 	setup() {
-		const email = ref('');
+		const usernameOrEmail = ref('');
 		const password = ref('');
 
-		return { email, password };
+		const isEmail = computed(() => validator.validate(usernameOrEmail.value));
+
+		const onLogin = async () => {
+			const response = await serverKy.post('auth/login', {
+				json: {
+					[isEmail.value ? 'email' : 'username']: usernameOrEmail.value,
+					password: password.value,
+				},
+			});
+			// TODO: store response in localstorage
+		};
+
+		return { usernameOrEmail, password, onLogin };
 	},
 });
 </script>
