d7bbfbd5f8f7840b1e56ad0b1a969ce3b78eaa98
Dragon <45088170+dragondevelops@users.noreply.github.com>
2021-09-04 16:42:21 -0400

    feat(frontend): login functionality

--- frontend/src/pages/account/login-page.vue
+++ frontend/src/pages/account/login-page.vue
@@ -2,6 +2,7 @@
 AuthLayout(title="Login to DrillDev")
 	AuthInputBox(title="Email", type="text", v-model="usernameOrEmail")
 	AuthInputBox(title="Password", type="password", v-model="password")
+	.text-red-500.font-bold {{ currentError }}
 	AuthButton.bg-blue-700.text-white.transition-all.duration-25(
 		title="Login",
 		class="hover:bg-blue-900",
@@ -11,7 +12,9 @@ AuthLayout(title="Login to DrillDev")
 
 <script lang="ts">
 import validator from 'email-validator';
+import type { HTTPError } from 'ky';
 import { computed, defineComponent, ref } from 'vue';
+import { useRouter } from 'vue-router';
 
 import AuthButton from '~/components/auth-button.vue';
 import AuthInputBox from '~/components/auth-input-box.vue';
@@ -22,22 +25,31 @@ export default defineComponent({
 	name: 'LoginPage',
 	components: { AuthLayout, AuthInputBox, AuthButton },
 	setup() {
+		const router = useRouter();
+
 		const usernameOrEmail = ref('');
 		const password = ref('');
 
 		const isEmail = computed(() => validator.validate(usernameOrEmail.value));
 
+		const currentError = ref('');
+
 		const onLogin = async () => {
-			const _response = await serverKy.post('auth/login', {
-				json: {
-					[isEmail.value ? 'email' : 'username']: usernameOrEmail.value,
-					password: password.value,
-				},
-			});
-			// TODO: store response in localstorage
+			try {
+				await serverKy.post('auth/login', {
+					json: {
+						[isEmail.value ? 'email' : 'username']: usernameOrEmail.value,
+						password: password.value,
+					},
+				});
+				router.push('/');
+			} catch (error) {
+				const httpError = error as HTTPError;
+				currentError.value = await httpError.response.text();
+			}
 		};
 
-		return { usernameOrEmail, password, onLogin };
+		return { usernameOrEmail, password, onLogin, currentError };
 	},
 });
 </script>
--- frontend/src/utils/ky.ts
+++ frontend/src/utils/ky.ts
@@ -2,4 +2,5 @@ import ky from 'ky';
 
 export const serverKy = ky.extend({
 	prefixUrl: import.meta.env.VITE_SERVER_URL,
+	credentials: 'include',
 });
