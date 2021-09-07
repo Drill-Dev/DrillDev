bd27ceb91ad42abff7f1beb44506ce25792b5e39
Leon Si <leonzalion@gmail.com>
2021-09-07 19:31:37 -0400

    fix: script

--- scripts/hooks/pre-commit.ts
+++ scripts/hooks/pre-commit.ts
@@ -6,8 +6,12 @@ function precommit() {
 	const branch = commandSync(
 		'git rev-parse --symbolic-full-name --abbrev-ref HEAD'
 	).stdout.toString();
+
+	console.log(branch);
 	if (branch === 'dev') {
-		commandSync('yarn run lint-staged');
+		commandSync('yarn run lint-staged', {
+			stdout: process.stdout,
+		});
 	}
 }
 
